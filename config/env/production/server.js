af79d6bc0d1465c79f9b581a8feb8716fc6664e5
m/s <m@umbr-ch.de>
2023-02-13 21:02:15 +0100

    remove localhost from config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,6 @@
 // Path: ./config/env/production/server.js
 
 module.exports = ({ env }) => ({
-  proxy: true,
   url: env("RENDER_EXTERNAL_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://localhost:1337",
-  webhooks: {
-    // Add this to not receive populated relations in webhooks
-    populateRelations: false,
-  },
+  // url: "http://localhost:1337",
+  // webhooks: {
+  //   // Add this to not receive populated relations in webhooks
+  //   populateRelations: false,
+  // },
   app: {
     keys: env.array("APP_KEYS"),
   },
