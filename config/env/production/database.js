2634be06e5eb6133fc97f8dac5a56d6da7edcd0e
m/s <m@umbr-ch.de>
2023-02-13 19:33:19 +0100

    minor changes database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,18 @@
 // path: ./config/env/production/database.js
 
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+// const config = parse(process.env.DATABASE_URL);
+const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: host,
+      port: port,
+      database: database,
+      user: user,
+      password: password,
       ssl: {
         rejectUnauthorized: false,
       },
